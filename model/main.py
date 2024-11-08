7113a7a25858e3c22df52d42611924d438ee9b9f
kpavan2004 <kpavan2004@gmail.com>
2024-11-08 15:33:55 +0000

    Final code changes after integrating with prometheus

--- model/main.py
+++ model/main.py
@@ -2,25 +2,74 @@
 import pickle
 import gradio as gr
 import pandas as pd
-from fastapi import FastAPI, Request, Response
+from prometheus_client import Counter, Histogram, generate_latest
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from typing import Any
+
+from fastapi import APIRouter, FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from sklearn.metrics import r2_score,accuracy_score, f1_score, precision_score, recall_score
+
+
+api_router = APIRouter()
+
+curr_path = str(Path(__file__).parent)
+
+title = "Patient Survival Prediction"
+description = "Predict survival of patient with heart failure, given their clinical record"
+
 
 # FastAPI object
 app = FastAPI()
 
-# Define Prometheus metrics
-REQUEST_COUNT = Counter(
-    'request_count', 
-    'Total number of requests',
-    ['method', 'endpoint']
-)
-REQUEST_LATENCY = Histogram(
-    'request_latency_seconds', 
-    'Latency of requests in seconds',
-    ['endpoint']
-)
+root_router = APIRouter()
+
+import prometheus_client as prom
+
 # Load the model from the .pkl file
 with open('xgboost-model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
+
+acc_metric = prom.Gauge('patient_survival_accuracy_score', 'Accuracy score for few random 100 test samples')
+f1_metric = prom.Gauge('patient_survival_f1_score', 'F1 score for few random 100 test samples')
+precision_metric = prom.Gauge('patient_survival_precision_score', 'Precision score for few random 100 test samples')
+recall_metric = prom.Gauge('patient_survival_recall_score', 'Recall score for few random 100 test samples')
+r2_metric = prom.Gauge('patient_survival_r2_score', 'R2 score for random 100 test samples')
+
+# LOAD TEST DATA
+test_data = pd.read_csv(curr_path + "/test_dataset.csv")
+
+# Function for updating metrics
+def update_metrics():
+    test = test_data.sample(10)
+    test_feat = test.drop('Death_event', axis=1)
+    test_cnt = test['Death_event'].values
+    test_pred = loaded_model.predict(test_feat)
+    
+    r2 = r2_score(test_cnt, test_pred)
+    r2_metric.set(r2)
+    
+    acc = accuracy_score(test_cnt, test_pred)
+    acc_metric.set(acc)
+
+    f1 = f1_score(test_cnt, test_pred)
+    f1_metric.set(f1)
+
+    precision = precision_score(test_cnt, test_pred)
+    precision_metric.set(precision)
+
+    recall = recall_score(test_cnt, test_pred)
+    recall_metric.set(recall)
+
+@app.get("/metrics")
+async def get_metrics():
+    update_metrics()
+    return Response(media_type="text/plain", content= prom.generate_latest())
+
     
 def predict_death_event(age,anaemia,creatinine_phosphokinase,diabetes,ejection_fraction,high_blood_pressure,platelets,serum_creatinine,serum_sodium,sex,smoking,time):
     age=float(age)
@@ -40,7 +89,6 @@ def predict_death_event(age,anaemia,creatinine_phosphokinase,diabetes,ejection_f
     input_df = pd.DataFrame([input],columns = ['age','anaemia','creatinine_phosphokinase','diabetes','ejection_fraction','high_blood_pressure','platelets','serum_creatinine','serum_sodium','sex','smoking','time'])
     pred_results = loaded_model.predict(input_df)
     return pred_results[0]
-
 
 age = gr.Slider(0, 100, label="age", value=70, info="Choose age between 0 and 100")
 anaemia = gr.Radio(["0", "1"], label="anaemia",value="0", info="Does patient have anaemia ->  0-False, 1-True")
@@ -74,25 +122,8 @@ iface = gr.Interface(fn = predict_death_event,
 # iface.launch(share = True,debug=True,server_name="0.0.0.0", server_port = 8001)  # server_name="0.0.0.0", server_port = 8001   # Ref: https://www.gradio.app/docs/interface
 
 # Mount gradio interface object on FastAPI app at endpoint = '/'
-app = gradio.mount_gradio_app(app, iface, path="/")
+app = gr.mount_gradio_app(app, iface, path="/")
 
-# Metrics route
-@app.get("/metrics")
-async def metrics(request: Request):
-    # Increment request count
-    REQUEST_COUNT.labels(method=request.method, endpoint=request.url.path).inc()
-
-    # Record the latency of the request
-    start_time = time.time()
-    response = Response(generate_latest(), media_type="text/plain")
-    process_time = time.time() - start_time
-
-    # Track the latency
-    REQUEST_LATENCY.labels(endpoint=request.url.path).observe(process_time)
-    
-    return response
-
-    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
